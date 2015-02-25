fe584a1e17ef01b2a397f53d502ee5d567db0c02
summer.zhu <summer.zhu@vipshop.com>
2015-02-25 10:32:42 +0800

    建立工程

---
+++ MLXC/MLXC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLXC
+//
+//  Created by summer.zhu on 25/2/15.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLXC/MLXC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MLXC
+//
+//  Created by summer.zhu on 25/2/15.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
